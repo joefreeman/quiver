78391b55f9e496eb292642a5094e0fcc795d4e5e
Joe Freeman <joe@joef.uk>
2025-09-10 00:26:08 +0100

    Use macro to define builtins

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -12,6 +12,24 @@ use std::collections::HashMap;
 pub mod io;
 pub mod math;
 
+/// Macro for building a module's builtin function registry
+macro_rules! builtin_module {
+    ($($fn_name:literal : $impl:path : [$($param:expr),*] -> [$($result:expr),*]),* $(,)?) => {{
+        let mut functions = HashMap::new();
+        $(
+            functions.insert(
+                $fn_name,
+                (
+                    $impl as BuiltinFn,
+                    &[$($param),*] as &[types::Type],
+                    &[$($result),*] as &[types::Type],
+                ),
+            );
+        )*
+        functions
+    }};
+}
+
 /// Function signature for builtin implementations
 pub type BuiltinFn = fn(&Value, &[Constant]) -> Result<Value, Error>;
 
@@ -69,61 +87,23 @@ impl BuiltinRegistry {
 fn create_builtin_registry() -> BuiltinRegistry {
     let mut modules = HashMap::new();
 
-    // Math module
-    let mut math_functions = HashMap::new();
-    math_functions.insert(
-        "abs",
-        (
-            math::builtin_math_abs as BuiltinFn,
-            &[types::Type::Integer] as &[types::Type],
-            &[types::Type::Integer] as &[types::Type],
-        ),
+    modules.insert(
+        "math",
+        builtin_module! {
+            "abs": math::builtin_math_abs: [types::Type::Integer] -> [types::Type::Integer],
+            "sqrt": math::builtin_math_sqrt: [types::Type::Integer] -> [types::Type::Integer],
+            "sin": math::builtin_math_sin: [types::Type::Integer] -> [types::Type::Integer],
+            "cos": math::builtin_math_cos: [types::Type::Integer] -> [types::Type::Integer],
+        },
     );
-    math_functions.insert(
-        "sqrt",
-        (
-            math::builtin_math_sqrt as BuiltinFn,
-            &[types::Type::Integer],
-            &[types::Type::Integer],
-        ),
-    );
-    math_functions.insert(
-        "sin",
-        (
-            math::builtin_math_sin as BuiltinFn,
-            &[types::Type::Integer],
-            &[types::Type::Integer],
-        ),
-    );
-    math_functions.insert(
-        "cos",
-        (
-            math::builtin_math_cos as BuiltinFn,
-            &[types::Type::Integer],
-            &[types::Type::Integer],
-        ),
-    );
-    modules.insert("math", math_functions);
 
-    // IO module
-    let mut io_functions = HashMap::new();
-    io_functions.insert(
-        "print",
-        (
-            io::builtin_io_print as BuiltinFn,
-            &[types::Type::Integer, types::Type::Binary] as &[types::Type],
-            &[types::Type::Tuple(TypeId::OK)] as &[types::Type],
-        ),
+    modules.insert(
+        "io",
+        builtin_module! {
+            "print": io::builtin_io_print: [types::Type::Integer, types::Type::Binary] -> [types::Type::Tuple(TypeId::OK)],
+            "println": io::builtin_io_println: [types::Type::Integer, types::Type::Binary] -> [types::Type::Tuple(TypeId::OK)],
+        }
     );
-    io_functions.insert(
-        "println",
-        (
-            io::builtin_io_println as BuiltinFn,
-            &[types::Type::Integer, types::Type::Binary] as &[types::Type],
-            &[types::Type::Tuple(TypeId::OK)] as &[types::Type],
-        ),
-    );
-    modules.insert("io", io_functions);
 
     BuiltinRegistry { modules }
 }
