8ccc7c3e3835b8d7e12000bee13256d46d1dba5f
Joe Freeman <joe@joef.uk>
2025-09-09 19:07:44 +0100

    Add test for operation member access

--- tests/member_access.rs
+++ tests/member_access.rs
@@ -2,8 +2,21 @@ mod common;
 use common::*;
 
 #[test]
-fn test_nested_member_access() {
+fn test_nested_member_access_as_value() {
     quiver()
         .evaluate("x = A[a: [10, B[b: 20]]], x.a.1.b")
         .expect_int(20);
+}
+
+#[test]
+fn test_nested_member_access_as_operation() {
+    quiver()
+        .evaluate(
+            r#"
+            inc = #int { [$, 1] ~> + },
+            x = [0, [f: inc]],
+            4 ~> x.1.f
+            "#,
+        )
+        .expect_int(5);
 }
