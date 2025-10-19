8abd805ba4a4642768446634dba60d918d18bdd3
Joe Freeman <joe@joef.uk>
2025-10-19 12:50:53 +0100

    Add build workflows

--- quiver-web/build.sh
+++ quiver-web/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -e
-
 cd "$(dirname "$0")"
 
-echo "Building Quiver WASM..."
-
-# Build the WASM module
-cargo build --target wasm32-unknown-unknown --release -p quiver-web
-
-# Generate bindings
-wasm-bindgen \
-    --target web \
-    --out-dir pkg \
-    ../target/wasm32-unknown-unknown/release/quiver_web.wasm
+wasm-pack build --target bundler --out-dir pkg
--- quiver-web/src/lib.rs
+++ quiver-web/src/lib.rs
@@ -4,3 +4,11 @@ mod worker_entry;
 
 pub use repl_web::Repl;
 pub use worker_entry::worker_main;
+
+use wasm_bindgen::prelude::*;
+
+/// Returns the version of the quiver-web package
+#[wasm_bindgen]
+pub fn version() -> String {
+    env!("CARGO_PKG_VERSION").to_string()
+}
